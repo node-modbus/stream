f9f05151d7a0f77229932179deb90262bf5d0fb4
Diogo Resende <dresende@thinkdigital.pt>
2017-06-27 18:06:17 +0100

    tcp: ensure tcp properly splits package

--- test/integration/quirks.js
+++ test/integration/quirks.js
@@ -1,15 +1,16 @@
 var assert = require("assert");
 var help   = require("../help");
+var debug  = true;
 
 describe("transport quirks", function () {
-	[ "serial", "tcp", "ascii" ].map(function (TRANSPORT) {
+	[ "serial", "ascii", "tcp" ].map(function (TRANSPORT) {
 		describe(TRANSPORT, function () {
 			var transport = new help.modbus.transports[TRANSPORT](help.stream(), {
 				retries : 0,
 				retry   : 5000
 			});
 			var stream    = new help.modbus.stream(transport, {
-				debug : TRANSPORT
+				debug : (debug ? TRANSPORT : null)
 			});
 
 			it("should invoke callback only once", function (done) {
@@ -44,6 +45,36 @@ describe("transport quirks", function () {
 				setTimeout(function () {
 					stream.transport.stream.push(res);
 				}, 2000);
+			});
+
+			if (TRANSPORT != "tcp") return;
+
+			it("should properly split data", function (done) {
+				this.timeout(5000);
+
+				var responses = 0;
+				var timer     = setTimeout(function () {
+					assert.equal(responses, 1, "Transport should properly split data");
+
+					return done();
+				}, 2000);
+
+				stream.readCoils({ address: 0, quantity: 1, extra: { transactionId: 10 } }, function (err) {
+					if (err) {
+						// timeout receiving response
+						clearTimeout(timer);
+
+						return done(err);
+					}
+
+					responses += 1;
+				});
+
+				var res = stream.transport.wrap(help.modbus.pdu.ReadCoils.Response.build([ 1 ]), { transactionId: 10 });
+
+				res = help.buffer.concat([ res, res, res ]);
+
+				stream.transport.stream.push(res);
 			});
 		});
 	});
