2efd2c437c22fed0c8bfe2891e23419ccec057d2
Diogo Resende <dresende@thinkdigital.pt>
2017-07-04 18:53:33 +0100

    serial: fixes dropping data with less than 4 bytes

--- test/integration/quirks.js
+++ test/integration/quirks.js
@@ -78,4 +78,30 @@ describe("transport quirks", function () {
 			});
 		});
 	});
+
+	describe("serial", function () {
+		var transport = new help.modbus.transports.serial(help.stream(), {
+			retries : 0,
+			retry   : 5000
+		});
+		var stream    = new help.modbus.stream(transport, {
+			debug : (debug ? "serial" : null)
+		});
+
+		it("should be able to merge several pieces of a package", function (done) {
+			var res = stream.transport.wrap(help.modbus.pdu.ReadCoils.Request.build(2, 4), { slaveId: 10 });
+
+			stream.on("read-coils", function (req, reply) {
+				assert.equal(req.slaveId, 10);
+				assert.equal(req.request.address, 2);
+				assert.equal(req.request.quantity, 4);
+
+				return done();
+			});
+
+			stream.transport.stream.push(res.slice(0, 2));
+			stream.transport.stream.push(res.slice(2, 4));
+			stream.transport.stream.push(res.slice(4));
+		});
+	});
 });
